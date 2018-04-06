ceccca7cad43a2319bb87bf4478d6f9c26109bf8
Enrique Escobar <Enrique.Escobar@genome.ulaval.ca>
2018-04-06 09:36:57 -0400

    Add files via upload

--- DataCampeR/Doc/Factors.R
+++ DataCampeR/Doc/Factors.R
@@ -37,3 +37,34 @@ summary(factor_speed_vector)
 # Is data analyst 2 faster than data analyst 5?
 compare_them <- factor_speed_vector[2] > factor_speed_vector[5] 
 compare_them
+## Subset of emails with big numbers: email50_big
+email50_big <- email50 %>%
+  filter(number == "big")
+  
+# Glimpse the subset
+glimpse(email50_big)
+## Table of number variable
+table(email50_big$number)
+
+# Drop levels
+email50_big$number <- droplevels(email50_big$number)
+
+# Another table of number variable
+table(email50_big$number)
+## Calculate median number of characters: med_num_char
+med_num_char <- median(email50$num_char)
+
+# Create num_char_cat variable in email50
+email50 <- email50 %>%
+  mutate(num_char_cat = ifelse(num_char < med_num_char, "below median", "at or above median"))
+  
+# Count emails in each category
+table(email50$num_char_cat)
+## Create number_yn column in email50
+email50 <- email50 %>%
+  mutate(number_yn = ifelse(number == "none", "no", "yes"))
+
+# Visualize number_yn
+ggplot(email50, aes(x = number_yn)) +
+  geom_bar()
+#
